7da989b5ec0f4f5ceeec59ff734c79fe514f544c
Miguel <miguelcastillo1000@gmail.com>
2021-09-21 21:08:52 -0400

    added problem of the day for september 21

---
+++ potd-09.21.js
@@ -0,0 +1,6 @@
+let contacts = ["Steve Smith", "Matt Davis", "Amy Linn"];
+let father = contacts[1];
+let contacts[0] = "Mark Williams";
+contacts.pop();
+contacts.push("Michelle Johnson");
+let numberOfContacts = contacts.length
