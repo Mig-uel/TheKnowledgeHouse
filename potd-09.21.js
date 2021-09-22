48facc783d0a1d7415d7afded2a6abcc3b0b3b88
Miguel <miguelcastillo1000@gmail.com>
2021-09-21 21:14:04 -0400

    fixed line 3.

--- potd-09.21.js
+++ potd-09.21.js
@@ -1,6 +1,9 @@
 let contacts = ["Steve Smith", "Matt Davis", "Amy Linn"];
 let father = contacts[1];
-let contacts[0] = "Mark Williams";
+contacts[0] = "Mark Williams";
 contacts.pop();
 contacts.push("Michelle Johnson");
 let numberOfContacts = contacts.length
+
+console.log(contacts)
+console.log(numberOfContacts)
