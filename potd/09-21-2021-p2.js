4eab68fa760ad14f2ead25975aa4316dd5ccb895
Miguel <miguelcastillo1000@gmail.com>
2021-09-21 21:29:52 -0400

    finished problem 2.

---
+++ potd/09-21-2021-p2.js
@@ -0,0 +1,23 @@
+/* HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <button onclick="addFruits()">Starbucks</button>
+    <script src="script.js"></script>
+  </body>
+</html>
+*/
+
+let starbucks = []
+
+function addFruits() {
+  let userInput = prompt("Welcome to Starbucks! \nEnter a fruit: ");
+  starbucks.push(userInput);
+  console.log(starbucks)
+}
