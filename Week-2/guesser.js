6f7123313a19d1c7df4f1c26d0911fbfeeed8c2c
Miguel <miguelcastillo1000@gmail.com>
2021-09-25 19:06:10 -0400

    fixed index.

--- Week-2/guesser.js
+++ Week-2/guesser.js
@@ -1,8 +1,8 @@
 function letterGuesser() {
   let arr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   let i = Math.floor(Math.random() * arr.length);
-  i+=1;
   let letter = arr[i];
+  i+=1
 
   let userInput = parseInt(prompt("You have received the letter " + letter + ". Guess the letter's index number: "));
 
