5185b7dc6249e8d7376250d6736da7512bdc91f1
Miguel <miguelcastillo1000@gmail.com>
2021-09-27 11:41:02 -0400

    fixes.

--- Week-2-Project/showDown.js
+++ Week-2-Project/showDown.js
@@ -1,6 +1,6 @@
 function fight() {
-  let monster = prompt("What is the name of the monster that you are fighting?")
-  let here = prompt("What is the name of the hero?")
+  let monster = prompt("What is the name of the monster that you are fighting?");
+  let hero = prompt("What is the name of the hero?");
 
   let villainHealth = 50;
   let heroHealth = 50;
@@ -8,49 +8,63 @@ function fight() {
   let heroDice = Math.floor(Math.random() * 20);
   let villainDice = Math.floor(Math.random() * 20);
 
-  let attack = 1
+  let attack = 0;
 
-  while (villainHealth === 0 || heroHealth === 9) {
+  while (heroHealth >= 0) {
     if (attack%2 === 0) {
       if (heroDice === 1 || heroDice === 2) {
-        heroHealth -= 5
-        alert("Oops! You accidentally stabed yourself. (-5 HP)");
-        attack += 1
+        heroHealth -= 5;
+        alert(`Oops! You accidentally stabbed yourself (-5 HP)\n${hero}'s health: ${heroHealth}\n${monster}'s health: ${villainHealth}`);
+        attack += 1;
+        console.log(`Attack #: ${attack} Hero Dice: ${heroDice}`);
       }
       else if (heroDice === 3 || heroDice === 10) {
-        alert("You missed! (0 HP)");
-        attack += 1
+        alert(`You missed! (0 HP)\n${hero}'s health: ${heroHealth}\n${monster}'s health: ${villainHealth}`);
+        attack += 1;
+        console.log(`Attack #: ${attack} Hero Dice: ${heroDice}`)
       }
       else if (heroDice === 11 || heroDice === 18) {
         villainHealth -= 10;
-        alert("Significant damage done! (-10 HP)");
+        alert(`Significant damage done! (-10 HP)\n${hero}'s health: ${heroHealth}\n${monster}'s health: ${villainHealth}`);
         attack += 1;
+        console.log(`Attack #: ${attack} Hero Dice: ${heroDice}`);
       }
       else if (heroDice === 19 || heroDice === 20) {
         villainHealth -= 20;
-        alert("Critical hit! (-20 HP)");
-        attack += 1
+        alert(`Critical hit! (-20 HP)\n${hero}'s health: ${heroHealth}\n${monster}'s health: ${villainHealth}`);
+        attack += 1;
+        console.log(`Attack #: ${attack} Hero Dice: ${heroDice}`);
       }
     }
     else {
       if (villainDice === 1 || villainDice === 2) {
-        heroHealth -= 5
-        alert("Oops! You accidentally stabed yourself. (-5 HP)");
-        attack += 1
+        villainHealth -= 5
+        alert(`Oops! You accidentally stabbed yourself (-5 HP)\n${monster}'s health: ${villainHealth}\n${hero}'s health: ${heroHealth}`);
+        attack += 1;
+        console.log(`Attack #: ${attack} Villain Dice: ${villainDice}`);
       }
       else if (villainDice === 3 || villainDice === 10) {
-        alert("You missed! (0 HP)");
-        attack += 1
+        alert(`You missed! (0 HP)\n${monster}'s health: ${villainHealth}\n${hero}'s health: ${heroHealth}`);
+        attack += 1;
+        console.log(`Attack #: ${attack} Villain Dice: ${villainDice}`);
       }
       else if (villainDice === 11 || villainDice === 18) {
         heroHealth -= 10;
-        alert("Significant damage done! (-10 HP)");
+        alert(`Significant damage done! (-10 HP)\n${monster}'s health: ${villainHealth}\n${hero}'s health: ${heroHealth}`);
         attack += 1;
+        console.log(`Attack #: ${attack} Villain Dice: ${villainDice}`);
       }
       else if (villainDice === 19 || villainDice === 20) {
         heroHealth -= 20;
-        alert("Critical hit! (-20 HP)");
-        attack += 1
+        alert(`Critical hit! (-20 HP)\n${monster}'s health: ${villainHealth}\n${hero}'s health: ${heroHealth}`);
+        attack += 1;
+        console.log(`Attack #: ${attack} Villain Dice: ${villainDice}`);
     }
   }
+  heroDice = Math.floor(Math.random() * 20);
+  villainDice = Math.floor(Math.random() * 20);
+  villainHealth = villainHealth;
+  heroHealth = heroHealth;
 }
+}
+fight();
--- Week-3-Project/loops.js
+++ Week-3-Project/loops.js
@@ -1,4 +1,5 @@
 // Part 1
+console.log("Part 1: ")
 function func(i) {
   console.log(i);
 }
@@ -10,8 +11,21 @@ for (let i = 1; i <= 10; i++) {
 function odds() {
   let arr = [];
   for (let i = 3; i <= 103; i++) {
-    if (i%3 === 0) {
-      
+    if (i%2 != 0) {
+      arr.push(i)
     }
   }
+  return arr;
 }
+console.log(`Part 2: ${odds()}`);
+
+// Part 3
+function arraySum(nums) {
+  let sum = 0;
+  for(let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+let numsArr = [1,2,3,4,5];
+console.log(`Part 3: ${arraySum(numsArr)}`);
