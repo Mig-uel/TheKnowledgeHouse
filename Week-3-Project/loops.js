c203e029dbc4fd095e3f2e9f27de0c863ed44766
Miguel <miguelcastillo1000@gmail.com>
2021-09-27 11:31:41 -0400

    started week 3.

---
+++ Week-3-Project/loops.js
@@ -0,0 +1,17 @@
+// Part 1
+function func(i) {
+  console.log(i);
+}
+for (let i = 1; i <= 10; i++) {
+  func(i);
+}
+
+//Part 2
+function odds() {
+  let arr = [];
+  for (let i = 3; i <= 103; i++) {
+    if (i%3 === 0) {
+      
+    }
+  }
+}
