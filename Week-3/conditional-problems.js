589e55cde9459a70cb350a65ccd4d85b0c35d9e4
Miguel <miguelcastillo1000@gmail.com>
2021-09-27 21:05:52 -0400

    finished part d

--- Week-3/conditional-problems.js
+++ Week-3/conditional-problems.js
@@ -12,5 +12,44 @@ for(let i = 1; i < lunchArray.length; i++) {
     temp = temp;
   }
 }
-
 console.log(`The longest word in the array is ${temp} and it is ${longest} words long!`)
+
+//Part B
+// Write a function that returns elements on odd positions in a list.
+
+// Part C
+function factorial(n) {
+  var fac = 1;
+
+  while(n != 0) {
+    fac *= n;
+    n--;
+  }
+  return fac;
+}
+console.log(`The factorial of 5 is ${factorial(5)}`);
+
+// Part D
+let mpg = 120;
+
+if (mpg >= 120) {
+  console.log("If you are using the Tom Ogle fuel system, props!")
+}
+else if (mpg > 35) {
+  console.log("The fish of the sea smile at your conservation!")
+}
+else if (mpg > 29) {
+  console.log("Not many gas stops will be taken!")
+}
+else if (mpg > 20) {
+  console.log("Atmosphere smiles at your decision!")
+}
+else if (mpg > 16) {
+  console.log("It's palpable!")
+}
+else if (mpg > 10) {
+  console.log("Planet still frowns upon this, and so does your wallet!")
+}
+else if (mpg <= 10) {
+  console.log("Gas guzzler!");
+}
