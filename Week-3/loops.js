cb53ca5d7de31618632b819a37af3cfaa8ab6427
Miguel <miguelcastillo1000@gmail.com>
2021-09-28 20:34:19 -0400

    added loops.js.

---
+++ Week-3/loops.js
@@ -0,0 +1,29 @@
+//Write a for loop that will iterate from 0 to 10,
+//it will multiply the number by 9
+//and log the result (e.g. "2 * 9 = 18")
+//Bonus: Use a nested for loop to show the tables for every
+//multiplier from 1 to 10 (100 results total)
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i} times 9 is ${i*9}`)
+}
+
+//Write a while loop that starts counting from 5.
+//Count up to (including) 50 and count only every fifth number
+//Don' forget to log it
+
+let i = 5
+while (i <= 50) {
+  console.log(`Every fifth number starting from 5 is ${i}`);
+  i+=5
+}
+
+//Write a function that takes an array of numbers as an argument.
+//This function should return the first duplicate number in the array:
+// For example: [2,7,5,9,8,7] should return 7;
+
+
+
+//Remembering the results from the conditionals exercise on Monday
+// use the assignGrade function by logging every value from 60 to 100:
+//your log should show "For 88, you got a B" "For 90 you got an A"etc..
