3566fdb0d978a2f52978f58130e885a611213395
Miguel <miguelcastillo1000@gmail.com>
2021-11-05 21:54:49 -0400

    got it to remove!

---
+++ Week-6/Flashcard-App/main.js
@@ -0,0 +1,35 @@
+function addCard() {
+  document.getElementById("add-card-btn").addEventListener("click", () => {
+    let cardForm = document.getElementById("create-card")
+    cardForm.style.display = "block";
+
+    let cancelButton = document.getElementById("cancel-btn")
+    cancelButton.addEventListener("click", () => { cardForm.style.display = "none" });
+  });
+
+  let createCard = document.getElementById("create-btn");
+  let parent = document.getElementById("flashcard-container");
+  let counter = 0;
+
+  createCard.addEventListener("click", () => {
+    counter += 1;
+
+    let question = document.getElementById("question-text").value;
+    let answer = document.getElementById("answer-text").value;
+    let card = document.createElement("li");
+
+    card.className = "flashcard";
+    card.id = counter;
+
+    card.innerHTML = `
+                        <h1>${question}</h1><br>
+                        <a id="toggle-${counter}" href="#">Show/Hide Answer</a><br><br>
+                        <p id="answer-${counter}">${answer}</p>
+                        <br><br>
+                        <button onclick="document.getElementById(${counter}).remove()" class="btn btn-danger type="button" id=delete-${counter}">Delete</button>
+                        <button class="btn btn-warning type="button" id=edit-${counter}">Edit</button>
+                      `;
+    parent.appendChild(card);
+  });
+}
+addCard();
